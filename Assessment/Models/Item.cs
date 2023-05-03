3926a75cb5dd49dd9e40a16605255ecc376743ca
jrendon1806 <jrendonc1806@outlook.com>
2023-05-03 08:44:06 -0600

    [FIX]:Remove unused files and references

---
+++ Assessment/Models/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media;
+
+namespace Assessment.ViewModels
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public Brush ColorCode { get; set; }
+        public ImageSource Image { get; set; }
+        public bool IsImageSelected { get; set; }
+        public bool IsPhotoSelected { get; set; }
+    }
+}
--- Assessment/Views/MainPage.xaml.cs
+++ Assessment/Views/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Assessment.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -26,5 +27,6 @@ namespace Assessment
         {
             this.InitializeComponent();
         }
+
     }
 }
